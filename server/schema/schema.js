e0e7419139a8500187e9c618826323eb72672da7
nasriime <mnasr4040@gmail.com>
2019-11-01 04:49:07 +0200

    add aws credentials upload mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,13 @@
 const graphql = require('graphql');
 const _ = require('lodash');
+const AWS = require('aws-sdk');
+const { GraphQLUpload } = 'graphql-upload';
+const fs = require('fs')
 const Item = require('../models/item');
+
+AWS.config.loadFromPath('../credentials.json');
+
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 const {
     GraphQLObjectType,
@@ -19,6 +26,15 @@ const ItemType = new GraphQLObjectType({
         type: { type: GraphQLString },
         price: {type: GraphQLString},
         photo: {type: GraphQLString},
+    })
+});
+
+const FileType = new GraphQLObjectType({
+    name: 'File',
+    fields:() =>({
+        filename:  { type: GraphQLString },
+        mimetype:  { type: GraphQLString },
+        encoding:  { type: GraphQLString },
     })
 });
 
@@ -60,6 +76,41 @@ const Mutation = new GraphQLObjectType({
                     photo: args.photo
                 });
                 return item.save();
+            }
+        },
+        singleUpload:{
+            type: FileType,
+            args:{
+                file :{ type: GraphQLUpload}
+            },
+            resolve(parent, args){
+                return args.file.then(file => {
+                    const {createReadStream, filename, mimetype} = file
+            
+                    const fileStream = createReadStream()
+            
+                    fileStream.pipe(fs.createWriteStream(`./uploadedFiles/${filename}`))
+            
+                    return file;
+                });
+            }
+        },
+        singleUploadStream:{
+            type: FileType,
+            args:{
+                file :{ type: GraphQLUpload}
+            },
+            async resolve(parent, args){
+                const file = await args.file
+                const {createReadStream, filename, mimetype} = file
+                const fileStream = createReadStream()
+            
+                const uploadParams = {Bucket: 'cafe-react-test', Key: filename, Body: fileStream};
+                const result = await s3.upload(uploadParams).promise()
+            
+                console.log(result)
+            
+                return file;
             }
         }
     }
